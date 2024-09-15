d055d6a8e778f6024336ee79c638dbf5a83df005
imer1319 <imer1319nd@gmail.com>
2024-09-14 22:55:00 -0300

    cambios en las facturas en el index

--- resources/views/admin/facturas/index.blade.php
+++ resources/views/admin/facturas/index.blade.php
@@ -21,14 +21,6 @@
         <div class="card card-primary card-outline">
             <div class="mx-3 my-2 d-flex align-items-center justify-content-between">
                 <h5>Listado de facturas</h5>
-                <div>
-                    <a href="{{ route('admin.liquidacion.download.excel') }}" class="btn btn-primary rounded-pill float-end">
-                        <i class="fas fa-file-excel"></i>
-                    </a>
-                    <a href="{{ route('admin.facturaciones.create') }}" class="btn btn-primary rounded-pill float-end">
-                        <i class="fa fa-plus"></i>
-                    </a>
-                </div>
             </div>
             <div class="card-body">
 
